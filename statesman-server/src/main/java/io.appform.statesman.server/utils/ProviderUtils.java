e6d5df4a60e2f898c57c3fcc5acd6ab1ab4b4a0f
Shivangi <shivangi.singh@phonepe.com>
2020-03-30 01:57:35 +0530

    provider related apis

--- statesman-server/src/main/java/io.appform.statesman.server/utils/ProviderUtils.java
+++ statesman-server/src/main/java/io.appform.statesman.server/utils/ProviderUtils.java
@@ -1,15 +1,26 @@
 package io.appform.statesman.server.utils;
 
 import io.appform.statesman.model.request.CreateProvider;
+import io.appform.statesman.model.response.ProviderInfo;
 import io.appform.statesman.server.dao.providers.StoredProvider;
 
 public class ProviderUtils {
 
-    public static StoredProvider toDao(CreateProvider request){
+    public static StoredProvider toDto(CreateProvider request){
         return StoredProvider.builder()
                 .providerId(request.getProviderId())
                 .providerName(request.getProviderName())
                 .partitions(request.getPartitions())
+                .active(true)
+                .build();
+    }
+
+    public static ProviderInfo toDto(StoredProvider storedProvider){
+        return ProviderInfo.builder()
+                .providerId(storedProvider.getProviderId())
+                .providerName(storedProvider.getProviderName())
+                .partitions(storedProvider.getPartitions())
+                .active(storedProvider.isActive())
                 .build();
     }
 
