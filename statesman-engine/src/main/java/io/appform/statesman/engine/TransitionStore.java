d7d7cde3d9b259792277e76dffeb75d59974b51c
Santanu Sinha <santanu.sinha@gmail.com>
2020-03-29 19:19:23 +0530

    Added some providers etc

---
+++ statesman-engine/src/main/java/io/appform/statesman/engine/TransitionStore.java
@@ -0,0 +1,7 @@
+package io.appform.statesman.engine;
+
+/**
+ *
+ */
+public interface TransitionStore {
+}
---
+++ statesman-model/src/main/java/io/appform/statesman/model/WorkflowTemplate.java
@@ -0,0 +1,7 @@
+package io.appform.statesman.engine;
+
+/**
+ *
+ */
+public class WorkflowTemplate {
+}
