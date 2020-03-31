ac93da89db0385856e558a1a821aeafcfce55b48
Santanu Sinha <santanu.sinha@gmail.com>
2020-03-31 18:38:27 +0530

    Introduced default state

--- statesman-model/src/main/java/io/appform/statesman/model/StateTransition.java
+++ statesman-model/src/main/java/io/appform/statesman/model/StateTransition.java
@@ -11,18 +11,25 @@ import javax.validation.Valid;
 @AllArgsConstructor
 public class StateTransition {
 
+    public static enum Type {
+        EVALUATED,
+        DEFAULT
+    }
+
     @Setter
     String id;
+
+    Type type;
 
     String fromState;
 
     boolean active;
 
-    @Valid
-    EvalRule rule;
+    String rule;
 
     @Valid
     State toState;
 
     String action;
+
 }
