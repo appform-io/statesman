cbd72dc9472150a371cd67d40ed2af93c465c7f0
tushar.mandar <tushar@phonepe.com>
2020-03-31 22:37:18 +0530

    removed array merge in DataMerge

--- statesman-engine/src/main/java/io/appform/statesman/engine/DataActionExecutor.java
+++ statesman-engine/src/main/java/io/appform/statesman/engine/DataActionExecutor.java
@@ -2,7 +2,6 @@ package io.appform.statesman.engine;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.appform.statesman.model.DataObject;
 import io.appform.statesman.model.DataUpdate;
@@ -34,19 +33,11 @@ public class DataActionExecutor {
             @Override
             public JsonNode visit(MergeDataAction mergeDataAction) {
                 ObjectNode objectNode = mapper.createObjectNode();
-                objectNode.setAll((ObjectNode)dataObject.getData());
+                objectNode.setAll((ObjectNode) dataObject.getData());
                 final ObjectNode updateData = (ObjectNode) dataUpdate.getData();
                 StreamSupport.stream(Spliterators.spliteratorUnknownSize(updateData.fields(), Spliterator.ORDERED), false)
                         .forEach(e -> {
-                            if(objectNode.has(e.getKey())) {
-                                val existingNode = objectNode.get(e.getKey());
-                                if(existingNode.isArray()) {
-                                    ((ArrayNode)existingNode).add(e.getValue());
-                                }
-                                else {
-                                    objectNode.set(e.getKey(), e.getValue());
-                                }
-                            } else {
+                            if (!objectNode.has(e.getKey())) {
                                 objectNode.set(e.getKey(), e.getValue());
                             }
                         });
@@ -56,11 +47,11 @@ public class DataActionExecutor {
             @Override
             public JsonNode visit(MergeSelectedDataAction mergeSelectedDataAction) {
                 ObjectNode objectNode = mapper.createObjectNode();
-                objectNode.setAll((ObjectNode)dataObject.getData());
+                objectNode.setAll((ObjectNode) dataObject.getData());
                 val updateNode = dataUpdate.getData();
                 mergeSelectedDataAction.getFields()
                         .forEach(field -> {
-                            if(!updateNode.has(field)) {
+                            if (!updateNode.has(field)) {
                                 throw new IllegalStateException(
                                         "Required field " + field + " not present in update for: " + dataUpdate.getWorkflowId());
                             }
