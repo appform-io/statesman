2250040c72fc843d3519ff430868d6cc23941099
tushar.mandar <tushar@phonepe.com>
2020-04-13 17:46:50 +0530

    code review fixes

--- statesman-engine/src/main/java/io/appform/statesman/engine/observer/events/WorkflowInitEvent.java
+++ statesman-engine/src/main/java/io/appform/statesman/engine/observer/events/WorkflowInitEvent.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class WorkflowInitEvent extends ObservableEvent {
     private Workflow workflow;
 
-    protected WorkflowInitEvent() {
+    public WorkflowInitEvent() {
         super(ObservableEventType.WORKFLOW_INIT);
     }
 
