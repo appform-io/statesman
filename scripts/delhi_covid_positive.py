94d5a9e9fec43846659dacd15df8e964857eeb59
tushar.mandar <tushar@phonepe.com>
2020-06-02 11:29:03 +0530

    added delhi covid positive script

---
+++ scripts/delhi_covid_positive.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+from os import listdir
+from os.path import isfile, join
+import csv
+import json
+import persistqueue
+import requests
+import shutil
+scanpath='/home/ganesh/uploads'
+processedPath='/home/ganesh/processed'
+rows = []
+csvFileNames = [f for f in listdir(scanpath) if isfile(join(scanpath, f))]
+jobQueue = persistqueue.UniqueAckQ('csv-processor')
+for csvFileName in csvFileNames:
+    fqFileName=scanpath + '/' + csvFileName
+    fqDestFilename=processedPath + '/' + csvFileName
+    try:
+        with open(fqFileName) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                try:
+                    convrow = dict((k.lower().replace(' ', '_'), v) for k,v in row.iteritems())
+                    convrow['body']['wfSource'] = 'delhi_covid_positive_csv'
+                    body = { 'id' : 'delhi_csv', 'body' : convrow, 'apiPath' : csvFileName }
+                    jobQueue.put(json.dumps(body))
+                    print('Queued job: ' + convrow['icmr_id'])
+                except:
+                    print('Error processing row: ' + str(row))
+    except:
+        print('Error processing file: ' + fqFileName)
+    print('Items queued for file [' + csvFileName + ']: ' + str(jobQueue.size))
+    shutil.move(fqFileName, fqDestFilename)
+    print('Moved file ' + fqFileName + ' to ' + fqDestFilename)
+print('Total queue size: ' + str(jobQueue.size))
+while jobQueue.size > 0:
+    payload = jobQueue.get()
+    icmrId = str(json.loads(payload)['body']['icmr_id'])
+    for i in range(3):
+        r = requests.post('http://localhost:8080/callbacks/ingress/raw/delhi_csv', data=payload, headers = {'content-type': 'application/json'})
+        if r.status_code == 200:
+            print('successfully posted data for icmr id: ' + icmrId)
+            jobQueue.ack(payload)
+            break
+        else:
+            print(str(i) + ': could not post data for icmr id: ' + icmrId + ' status:' + str(r.status_code))
+            if i == 2:
+                print('Failing for icmr id: ' + icmrId)
+                jobQueue.ack_failed(payload)
+            else:
+                print('Retrying for icmr id: ' + icmrId)
+                jobQueue.nack(payload)
+                break
+shutil.rmtree('csv-processor')
+print('Processing complete')
