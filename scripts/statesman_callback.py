dc184ba6f01c5627bb113b162b322d6dfb96855d
tushar.mandar <tushar@phonepe.com>
2020-08-24 23:54:43 +0530

    updated script

--- scripts/statesman_callback.py
+++ scripts/statesman_callback.py
@@ -112,6 +112,9 @@ CALLBACK_TEMPLATE = {
     "HI_PONDY_IVR_ATTEMPT_3": {
         "sql": "select workflow_id,current_state from workflow_instances where current_state IN ('HI_PONDY_IVR_ATTEMPT_3') and template_id = 'fd20aa74-3210-4761-9e3b-4d6ab43477fb' ",
         "callback_payload": {"status": "noanswer"}},
+    "HI_PONDY_ONBOARD_DOCTOR_FOLLOW": {
+        "sql": "select workflow_id,current_state from workflow_instances where current_state IN ('HI_ONBOARD_DOCTOR_FOLLOW') and template_id = 'fd20aa74-3210-4761-9e3b-4d6ab43477fb' ",
+        "callback_payload": {"dayEnd": True}},
     "HI_VOILATION": {
         "sql": "select workflow_id,current_state from workflow_instances where current_state IN ('HI_VOILATION')",
         "callback_payload": {"dayEnd": True}}
